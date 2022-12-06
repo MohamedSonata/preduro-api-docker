30836025c60e87867c2b0ea68272354312eb19d5
MohamedSonata <sonata.1993@yahoo.com>
2022-12-06 03:56:14 +0200

    env file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -14,7 +14,7 @@ module.exports = ({ env }) => {
           password: env('DATABASE_PASSWORD', 'strapi'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
           ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            rejectUnauthorized: false, // For self-signed certificates
           },
         },
         debug: false,
