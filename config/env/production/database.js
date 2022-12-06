1495b46a838fe2e4de597d7208779a5a748ded9b
MohamedSonata <sonata.1993@yahoo.com>
2022-12-06 02:48:42 +0200

    ssl data base

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,13 @@
 
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = env('DATABASE_URL');
+  // const { host, port, database, user, password } = env('DATABASE_URL');
 
   return {
     connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', '172.17.0.2'),
+          host: env('DATABASE_HOST', '127.0.0.1'),
           port: env.int('DATABASE_PORT',5432 ),
           database: env('DATABASE_NAME', 'strapi'),
           user: env('DATABASE_USERNAME', 'strapi'),
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,13 @@
 module.exports = ({ env }) => ({
-    url: env("RENDER_EXTERNAL_URL"),
-    app: {
-        keys: env.array('APP_KEYS'),
-      },
-  });
+    proxy: true,
+    url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
+});
+// module.exports = ({ env }) => ({
+//     url: env("RENDER_EXTERNAL_URL"),
+//     app: {
+//         keys: env.array('APP_KEYS'),
+//       },
+//   });
