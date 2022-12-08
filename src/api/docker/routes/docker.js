c069228e476bdd8ba305840be4fa9b073be6fb3d
MohamedSonata <sonata.1993@yahoo.com>
2022-12-08 05:18:49 +0200

    Src editing and Update Config delay 120s

--- src/api/docker/routes/docker.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * docker router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::docker.docker');
--- src/api/docker/services/docker.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * docker service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::docker.docker');
