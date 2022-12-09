4c8b4c65147becd9baf2c93c401257e283106bfb
MohamedSonata <sonata.1993@yahoo.com>
2022-12-09 12:20:59 +0200

    production server file

--- config/server.js
+++ config/server.js
@@ -1,18 +1,15 @@
- const isProduction = process.env.NODE_ENV === 'production';
-
- const isDevelopment = process.env.NODE_ENV === 'development';
-
- const port = isProduction ? '1338':
-           '1337';
-
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port:port,
-
-  // port:env('NODE_ENV')? 'production' = env.int('PROD_PORT','1338') : env.int('PORT','1337') ,
+  port: env.int('PORT', 1337),
 
   app: {
-    keys: ['myKeyA', 'myKeyB'],
+    keys: env.array('APP_KEYS'),
   },
 });
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// const isDevelopment = process.env.NODE_ENV === 'development';
+
+// const port = isProduction ? '1338'
+//           '1337'
+//            ;
