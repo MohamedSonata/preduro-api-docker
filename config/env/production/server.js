013fb62813c24df523933834c715a0a4a87598fb
MohamedSonata <sonata.1993@yahoo.com>
2022-12-09 11:04:20 +0200

    production server file

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,14 +1,13 @@
-module.exports = ({ env }) => ({
-    proxy: true,
-    host: env('HOST'), 
-    port:env.int('PROD_PORT','1338'),// replaces `host` and `port` properties in the development environment
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
-});
-// module.exports = ({ env }) => ({
-//     url: env("RENDER_EXTERNAL_URL"),
-//     app: {
-//         keys: env.array('APP_KEYS'),
-//       },
-//   });
+    // path: ./config/env/production/server.js
+
+    module.exports = ({ env }) => ({
+        proxy: true,
+        url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+        app: { 
+          keys: env.array('APP_KEYS')
+        },
+    });
+
+ 
+
+ 
--- config/server.js
+++ config/server.js
@@ -2,12 +2,12 @@
 
  const isDevelopment = process.env.NODE_ENV === 'development';
 
- const port = isProduction ? '1338'
-            : '1337';
+ const port = isProduction ? '1338':
+           '1337';
 
 
 module.exports = ({ env }) => ({
-  host: env('HOST'),
+  host: env('HOST', '0.0.0.0'),
   port:port,
 
   // port:env('NODE_ENV')? 'production' = env.int('PROD_PORT','1338') : env.int('PORT','1337') ,
