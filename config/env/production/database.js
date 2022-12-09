1ba98279524d5430cc9f529ad4a5891c61095958
MohamedSonata <sonata.1993@yahoo.com>
2022-12-09 16:38:33 +0200

    pro

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,11 +7,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT',5432 ),
-          database: env('DATABASE_NAME', 'postgres'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'postgrespw'),
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', ),
+          database: env('DATABASE_NAME', ''),
+          user: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''),
           schema: env('DATABASE_SCHEMA', ''), // Not required
           ssl: env.bool('DATABASE_SSL', false)
         },
