514d6d2a5627d4b5fa793a0eb7a2742181ab65b1
MohamedSonata <sonata.1993@yahoo.com>
2022-12-09 05:16:22 +0200

    production server file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,11 +7,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', ),
-          database: env('DATABASE_NAME', ''),
-          user: env('DATABASE_USERNAME', ''),
-          password: env('DATABASE_PASSWORD', ''),
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT' ),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
           // schema: env('DATABASE_SCHEMA', ''), // Not required
           ssl: env.bool('DATABASE_SSL', false)
         },
@@ -31,7 +31,6 @@ module.exports = ({ env }) => ({
   
 });
 
-// refrance
 
 
 // module.exports = ({ env }) => ({
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,11 @@
-// module.exports = ({ env }) => ({
-//     // proxy: true,
-//     host: env('HOST'), 
-//     port:env.int('PROD_PORT','1338'),// replaces `host` and `port` properties in the development environment
-//     app: { 
-//       keys: env.array('APP_KEYS')
-//     },
-// });
+module.exports = ({ env }) => ({
+    proxy: true,
+    host: env('HOST'), 
+    port:env.int('PROD_PORT','1338'),// replaces `host` and `port` properties in the development environment
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
+});
 // module.exports = ({ env }) => ({
 //     url: env("RENDER_EXTERNAL_URL"),
 //     app: {
