8fe54d7618a0c3ccfdd413e77c7e9e5aad67626b
MohamedSonata <sonata.1993@yahoo.com>
2022-12-09 19:30:32 +0200

    port condition

--- config/server.js
+++ config/server.js
@@ -1,15 +1,15 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const port = isProduction ? '1338':
+          '1337';
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', port),
 
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const isDevelopment = process.env.NODE_ENV === 'development';
-
-// const port = isProduction ? '1338'
-//           '1337'
-//            ;
