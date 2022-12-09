bdf420e5df2639189e860d5543787ea717ecd666
MohamedSonata <sonata.1993@yahoo.com>
2022-12-09 17:19:28 +0200

    production server file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', 'localhost'),
+          host: env('DATABASE_HOST', '127.0.0.1'),
           port: env.int('DATABASE_PORT', ),
           database: env('DATABASE_NAME', ''),
           user: env('DATABASE_USERNAME', ''),
