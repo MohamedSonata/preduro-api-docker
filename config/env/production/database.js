11697d6560ceac7e6f825584d10c88128e5e9fd3
MohamedSonata <sonata.1993@yahoo.com>
2022-12-10 14:58:04 +0200

    new compose dev image

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,3 @@
-
-
 module.exports = ({ env }) => ({
   // const { host, port, database, user, password } = env('DATABASE_URL');
 
@@ -16,14 +14,16 @@ module.exports = ({ env }) => ({
           ssl: env.bool('DATABASE_SSL', false)
         },
         debug: false,
+        // this pool Param is needed while using Docker Compose and docker Swarm for scalling the app
+         //to overrride database default Values
         pool: {
-          min: 0,
-          max: 10,
-          acquireTimeoutMillis: 600000,
-          createTimeoutMillis: 30000,
-          idleTimeoutMillis: 20000,
-          reapIntervalMillis: 20000,
-          createRetryIntervalMillis: 200
+          min: 0,// minimum clients to keep database container alive
+          max: 10,// Maximum number of database connections to keepalive
+          acquireTimeoutMillis: 600000, // 	Time in milliseconds before timing out a database connection attempt
+          createTimeoutMillis: 30000, // 	Time in milliseconds before timing out a create query attempt
+          idleTimeoutMillis: 20000, //Time in milliseconds before free database connections are destroyed
+          reapIntervalMillis: 20000, // 	Time in milliseconds to check for idle database connections to destroy
+          createRetryIntervalMillis: 200 //Time in milliseconds to idle before retrying failed create actions
         
         },
     
